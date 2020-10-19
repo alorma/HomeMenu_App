8ee7c1338da937efab3cf91c251bcf15710a14e7
Bernat Borrás <bernat.borras@adevinta.com>
2020-10-19 09:37:19 +0200

    Extract body corner

--- app/src/main/java/com/alorma/homemenu/MainActivity.kt
+++ app/src/main/java/com/alorma/homemenu/MainActivity.kt
@@ -3,17 +3,10 @@ package com.alorma.homemenu
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -23,6 +16,7 @@ import com.alorma.homemenu.main.MainViewModel
 import com.alorma.homemenu.time.Clock
 import com.alorma.homemenu.ui.HomeMenuTheme
 import com.alorma.homemenu.widgets.daysList
+import com.alorma.homemenu.widgets.shapeBodyContent
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.time.LocalDate
@@ -63,8 +57,10 @@ fun HomeScreen(mainViewModel: MainViewModel) {
         },
         floatingActionButtonPosition = FabPosition.End,
         bodyContent = {
-            homeComponent(mainViewModel) { day ->
-                mainViewModel.onDayClicked(day = day)
+            shapeBodyContent {
+                homeComponent(mainViewModel) { day ->
+                    mainViewModel.onDayClicked(day = day)
+                }
             }
         }
     )
@@ -77,22 +73,11 @@ private fun homeComponent(
     onDayClick: (Day) -> Unit,
 ) {
     val items = mainViewModel.days().collectAsState().value
-    Surface(
-        modifier = Modifier.clip(
-            MaterialTheme.shapes.medium.copy(
-                topLeft = CornerSize(16.dp),
-                topRight = CornerSize(0.dp),
-                bottomLeft = CornerSize(0.dp),
-                bottomRight = CornerSize(0.dp)
-            )
-        ).fillMaxSize()
-            .background(MaterialTheme.colors.background),
-    ) {
-        if (items.isEmpty()) {
-            Text("No items")
-        } else {
-            daysList(days = items, onDayClick = onDayClick)
-        }
+
+    if (items.isEmpty()) {
+        Text("No items")
+    } else {
+        daysList(days = items, onDayClick = onDayClick)
     }
 }
 
--- app/src/main/java/com/alorma/homemenu/widgets/ShapeBodyContent.kt
+++ app/src/main/java/com/alorma/homemenu/widgets/ShapeBodyContent.kt
@@ -1,4 +1,67 @@
 package com.alorma.homemenu.widgets
 
-class ShapeBodyContent {
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
+import com.alorma.homemenu.R
+import com.alorma.homemenu.ui.HomeMenuTheme
+
+@Composable
+fun shapeBodyContent(
+    shape: Shape? = null,
+    content: @Composable () -> Unit,
+) {
+    val bodyShape: Shape = shape ?: MaterialTheme.shapes.large.copy(
+        topLeft = CornerSize(16.dp),
+        topRight = CornerSize(0.dp),
+        bottomLeft = CornerSize(0.dp),
+        bottomRight = CornerSize(0.dp)
+    )
+
+    Surface(
+        modifier = Modifier
+            .clip(shape = bodyShape)
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background),
+        content = content,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun preview() {
+    HomeMenuTheme {
+        Scaffold(
+            backgroundColor = MaterialTheme.colors.primarySurface,
+            topBar = {
+                val title = stringResource(id = R.string.app_name)
+                TopAppBar(
+                    elevation = 0.dp,
+                    title = { Text(text = title) }
+                )
+            },
+            floatingActionButton = {
+                ExtendedFloatingActionButton(
+                    text = { Text("Add") },
+                    shape = RoundedCornerShape(percent = 50),
+                    onClick = { },
+                )
+            },
+            floatingActionButtonPosition = FabPosition.End,
+        ) {
+            shapeBodyContent {
+                Text("No items")
+            }
+        }
+    }
 }
