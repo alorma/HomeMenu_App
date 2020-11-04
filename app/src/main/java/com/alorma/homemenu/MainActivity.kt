3da789e5f075ce8cfd0cffa8330c4a4f66531389
Bernat Borrás <bernat.borras@adevinta.com>
2020-11-04 05:45:33 +0100

    Add icon to toolbar

--- app/src/main/java/com/alorma/homemenu/MainActivity.kt
+++ app/src/main/java/com/alorma/homemenu/MainActivity.kt
@@ -6,80 +6,103 @@ import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
-import com.alorma.homemenu.base.StringsProvider
 import com.alorma.homemenu.main.Day
 import com.alorma.homemenu.main.MainViewModel
-import com.alorma.homemenu.time.Clock
 import com.alorma.homemenu.ui.HomeMenuTheme
 import com.alorma.homemenu.widgets.daysList
-import com.alorma.homemenu.widgets.shapeBodyContent
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.time.LocalDate
 
 @ExperimentalCoroutinesApi
 class MainActivity : AppCompatActivity() {
 
     private val mainViewModel by viewModel<MainViewModel>()
 
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HomeMenuTheme {
-                HomeScreen(mainViewModel)
+                HomeScreen()
             }
         }
     }
 }
 
+@ExperimentalMaterialApi
 @ExperimentalCoroutinesApi
 @Composable
-fun HomeScreen(mainViewModel: MainViewModel) {
-    Scaffold(
-        backgroundColor = MaterialTheme.colors.primarySurface,
-        topBar = {
+fun HomeScreen() {
+    val bodyShape: Shape = MaterialTheme.shapes.large.copy(
+        topLeft = CornerSize(16.dp),
+        topRight = CornerSize(0.dp),
+        bottomLeft = CornerSize(0.dp),
+        bottomRight = CornerSize(0.dp)
+    )
+    val backDropState = rememberBackdropScaffoldState(BackdropValue.Concealed)
+    BackdropScaffold(
+        scaffoldState = backDropState,
+        appBar = {
             val title = stringResource(id = R.string.app_name)
             TopAppBar(
-                elevation = 0.dp,
+                navigationIcon = { navigationIcon(backDropState) },
                 title = { Text(text = title) }
             )
         },
-        floatingActionButton = {
-            ExtendedFloatingActionButton(
-                text = { Text("Add") },
-                shape = MaterialTheme.shapes.small.copy(all = CornerSize(8.dp)),
-                onClick = { mainViewModel.onAddNewDay() },
-            )
-        },
-        floatingActionButtonPosition = FabPosition.End,
-        bodyContent = {
-            shapeBodyContent {
-                homeComponent(mainViewModel) { day ->
-                    mainViewModel.onDayClicked(day = day)
-                }
+        backLayerBackgroundColor = MaterialTheme.colors.primarySurface,
+        backLayerContent = {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                alignment = Alignment.Center
+            ) {
+                Text("back layer")
             }
+        },
+        frontLayerShape = bodyShape,
+    ) {
+        homeComponent(data = MutableStateFlow(emptyList()), onDayClick = {})
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+private fun navigationIcon(backDropState: BackdropScaffoldState) {
+    IconButton(onClick = {
+        if (backDropState.isRevealed) {
+            backDropState.conceal()
+        } else {
+            backDropState.reveal()
         }
-    )
+    }) {
+        val asset = if (backDropState.isRevealed) {
+            vectorResource(id = R.drawable.ic_nav_expand_conceal)
+        } else {
+            vectorResource(id = R.drawable.ic_nav_expand_reveal)
+        }
+        Icon(asset = asset)
+    }
 }
 
 @ExperimentalCoroutinesApi
 @Composable
 private fun homeComponent(
-    mainViewModel: MainViewModel,
+    data: StateFlow<List<Day>>,
     onDayClick: (Day) -> Unit,
 ) {
-    val items = mainViewModel.days().collectAsState().value
+    val items = data.collectAsState().value
 
     if (items.isEmpty()) {
         Box(
@@ -93,18 +116,12 @@ private fun homeComponent(
     }
 }
 
+@ExperimentalMaterialApi
 @ExperimentalCoroutinesApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    val viewModel = MainViewModel(
-        object : Clock {
-            override suspend fun getToday(): LocalDate = LocalDate.now()
-            override suspend fun now(): Long = System.currentTimeMillis()
-        },
-        StringsProvider(ContextAmbient.current)
-    )
     HomeMenuTheme {
-        HomeScreen(viewModel)
+        HomeScreen()
     }
 }
